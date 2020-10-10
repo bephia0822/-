ead9bae684f7a3f35228deb898cdfbb16ed41fa1
bephia0822 <bephia0822@gmail.com>
2020-10-11 00:23:19 +0900

    Initial Commit

---
+++ 映画の選択/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  映画の選択
+//
+//  Created by Sophia Wang on 2020/10/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
