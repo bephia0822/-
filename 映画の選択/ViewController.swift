49aba4a7054be3e4d6465449da32c1c5ac3925cb
bephia0822 <bephia0822@gmail.com>
2021-05-06 21:39:00 +0900

    change background color

--- 映画の選択/ViewController.swift
+++ 映画の選択/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var movieView: UIImageView!
+    
+    
+    let movieArray = [#imageLiteral(resourceName: "天空の城"), #imageLiteral(resourceName: "崖上的波妞"), #imageLiteral(resourceName: "千と千尋"), #imageLiteral(resourceName: "猫の恩返し"), #imageLiteral(resourceName: "進撃の巨人3"), #imageLiteral(resourceName: "鬼滅の刃")]
+    
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func startButton(_ sender: UIButton) {
+        
+        movieView.image = movieArray[Int.random(in: 0...5)]
     }
-
-
+    
+    
 }
 
